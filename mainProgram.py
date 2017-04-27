709b092ecd2dd0027da09e08c6a81e1be842a005
moraleshugo <moraleshugo12@yahoo.com>
2017-04-26 21:51:00 -0700

    Add files via upload

--- mainProgram.py
+++ mainProgram.py
@@ -1,12 +1,47 @@
 # CIS320 Final Project
-# Gustavo, Hugo & Seth
+# Hugo & Seth
 # SB 4/20
 
-import mainMenu
+#import mainMenu, validate, sqlite3
+
+def getEmployeeNumber():
+    while True:
+        try:
+            userLogin = int(input("Please enter your Employee Number: "))
+        except ValueError:
+            print("Sorry Invalid Input.")
+            return getEmployeeNumber()
+
+        if userLogin < 1:
+            print("Sorry Invalid Input.")
+            return getEmployeeNumber()
+
+        elif userLogin > 100:
+            print("Sorry Invalid Input.")
+            return getEmployeeNumber()
+        else:
+            break
+    return userLogin
+
+getEmployeeNumber()
+
+
+
+
+
+
+
+#Ask for User Employee ID#
+#def inputUserLogin():
+   # userLogin = int(input("Please Enter your Employee Number: "))
+    #return userLogin
+
+
+
+
+
 
 #managerFlag = setManagerFlag( userLogin() )
 
-managerFlag = True
-
-mainMenu(managerFlag)
+#mainMenu(managerFlag)
 
