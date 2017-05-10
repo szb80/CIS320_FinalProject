3475e619ab1bfe220b52ed3880ac7bde65ae1832
GJC28 <gjc@fpu.edu>
2017-05-10 13:44:18 -0700

    Update POSModule.py

--- POSModule.py
+++ POSModule.py
@@ -77,7 +77,7 @@ def displayPosMenu():
         if int(menuChoice = True # passed all tests and exits loop
 
         # input passes all test; search line items and print result
-        if int(menuChoice) == 1:
+        if int(menuChoice) == 1: 
                if makeSale():
                    print("Item selected successfully")
                else: # default case
@@ -105,4 +105,69 @@ def searchSales(lineItemNum):
             return salesSQL.creatSalesFromSQLRecord(lineItemNum)
 
         return None
+
+
+def displaySaleNumber():
+    # displays all sale records in the database
+    return salesSQL.saleNumber()
+
+
+def modifySaleLineItemQty():
+    # adjsuts sale item quantity
+    isValid = False # initialize menu sentinel
+
+    print("Adjusted item quantity", "-" * SPACER_SIZE)
+
+    # start menu loop
+    while not isValid:
+        lineItemQty = input("Enter the quantity of the item you want to order: ")
+        # check if quantity is available and loop until valid
+        if not Validate.validateInt(lineItemQty):
+            print("**ERROR: select valid amount.")
+            continue
+
+        # check if both criteria are met and exit loop
+        if not salesSQL.searchForSQLRecord(lineItemQty) \
+               and Validate.validateInt(lineItemQty):
+            isValid = True
+        
+    # begin taking input for item:
+    lineItemNum = str("Enter the item you wish to purchase: ") # take name of item
+
+    lineItemQty = str("Enter the quantity: ") # take the amount of the item
+
+    # make a new sale record
+    newSaleNumber = salesSQL.createSQLRecord(newSaleNumber) ##### NOT SURE HOW TO COMPLETE THIS RECORD!!!
+
+    # attempt write and return status
+    return saleSQL.createSQLRecord(newSaleNumber)
+
+
+    # print copy of current record in db
+    print("Current Record:\n")
+    salesSQL.displayRecord(saleItemNum)
+
+def deleteSaleNumber():
+    # deletes an existing order
+    isValid = False
+
+    # Validates.cls()
+
+    print("DELETING ORDER ", "-" * SPACER_SIZE)
+
+    # loop menu until valid input is entered
+    while not isValid:
+        saleNumber = input("Enter the sale number to delete: [0 to cancel] " )
+        # check if saleNumber is a number and loop until valid
+        if not Validate.validateInt(saleNum):O
+            print("**ERROR: must be a number.")
+            continue
+        if int(saleNum) == 0:
+            return False
+        # check if sale number is still in database and loop until valid
+        if salesSQL.searchForSQLRecord(saleNum) \
+               and Validate.validateInt(saleNum):
+            isValid = True
+
+    return salesSQL.deleteSQLRecord(saleNum)
 
